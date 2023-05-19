df4488e2f3b8e3905b14f67e780c99a9e8696909
OmarHamada1996 <omer.jasser@gmail.com>
2023-05-19 15:18:19 +0000

    Adding href

--- tourapp/src/components/footer/Footer.js
+++ tourapp/src/components/footer/Footer.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2023 Travel Destination. All rights reserved.</p>
+      <div>
+      <ul>
+        <li><a href='#'>Facebook</a></li>
+        <li><a href='#'>Instgram</a></li>
+        <li><a href='#'>Twitter</a></li>
+      </ul>
+      </div>
+      <p>&copy; Hamada's Travel Agency. All rights reserved.</p>
     </footer>
   );
 };
